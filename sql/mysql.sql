55b2cd8bf2aee7f73c7da6d8946fce81d1f06f78
Joe Murphy <joe.murphy@gmail.com>
2018-07-17 13:28:33 -0400

    Clarify table names...
    ...for use in databases that have other unrelated tables in 'em

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -10,16 +10,10 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Database: `report-card`
+-- Table structure for table `report_cards`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `cards`
---
-
-CREATE TABLE IF NOT EXISTS `cards` (
+CREATE TABLE IF NOT EXISTS `report_cards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `slug` varchar(100) COLLATE utf8_bin NOT NULL,
   `group_slug` varchar(100) COLLATE utf8_bin NOT NULL,
@@ -31,18 +25,14 @@ CREATE TABLE IF NOT EXISTS `cards` (
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `cards`
---
-
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `grades`
+-- Table structure for table `report_grades`
 --
 
-CREATE TABLE IF NOT EXISTS `grades` (
+CREATE TABLE IF NOT EXISTS `report_grades` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cards_id` int(11) NOT NULL,
   `grade` tinyint(4) NOT NULL,
@@ -50,7 +40,3 @@ CREATE TABLE IF NOT EXISTS `grades` (
   UNIQUE KEY `id` (`id`),
   KEY `cards_id` (`cards_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `grades`
---
