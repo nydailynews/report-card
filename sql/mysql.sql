9f46411af987bc46944dfc0c2fc1b8da5f4fd159
Joe Murphy <joe.murphy@gmail.com>
2018-07-18 15:31:38 -0400

    Update mysql.sql

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -13,15 +13,16 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `report_cards`
 --
 
-CREATE TABLE IF NOT EXISTS `report_cards` (
+CREATE TABLE IF NOT EXISTS `report_card` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `slug` varchar(100) COLLATE utf8_bin NOT NULL,
   `group_slug` varchar(100) COLLATE utf8_bin NOT NULL,
   `title` varchar(250) COLLATE utf8_bin NOT NULL,
   `description` text COLLATE utf8_bin NOT NULL,
-  `date_expire` date DEFAULT NULL,
-  `grade_average` float NOT NULL,
+  `grade_mean` float NOT NULL,
+  `grade_median` float NOT NULL,
   `grades` int(11) NOT NULL,
+  `date_created` DATE NOT NULL,
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
@@ -32,9 +33,9 @@ CREATE TABLE IF NOT EXISTS `report_cards` (
 -- Table structure for table `report_grades`
 --
 
-CREATE TABLE IF NOT EXISTS `report_grades` (
+CREATE TABLE IF NOT EXISTS `report_card_grade` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cards_id` int(11) NOT NULL,
+  `card_id` int(11) NOT NULL,
   `grade` tinyint(4) NOT NULL,
   `ip` varchar(30) COLLATE utf8_bin NOT NULL,
   UNIQUE KEY `id` (`id`),
